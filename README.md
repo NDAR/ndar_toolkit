1c08a43ca9d7480e2514b45bccefb8841efaced2
David Obenshain <David.Obenshain@Gmail.com>
2015-06-08 16:22:57 +0000

    Updated for better compatability with Windows and multiple python versions

--- README.md
+++ README.md
@@ -12,21 +12,34 @@ Scripts and Instructions to aid in automating integration with NDAR tools and da
 1. Downloads NDAR command-line download manager, used to generate aws credentials from an NDAR username/password.
 2. Downloads s3cmd from s3tools.org (https://github.com/s3tools/s3cmd)
 3. Writes configuration files for s3cmd and for Amazon's command-line tool, using credentials generated from the download manager
+4. s3cmd requires Python 2.6 or higher, and requires some prerequisite libraries; tested with Python2.7
 
 To Use (run each time you want new keys):
 ```shell
-sudo python ndar_update_keys.py
+python ndar_update_keys.py
 ```
 
 After initial run:
-```shell
-cd s3cmd-master
-python setup.py install
-```
 
+1. On Linx/Unix
+  ```shell
+  cd ~/ndar_toolkit/s3cmd-master
+  python setup.py install
+  ```
+2. On Windows
+  ```shell
+  cd %HOME%/ndar_toolkit/s3cmd-master
+  python setup.py install
+  ```
+
+ 
 Now you can use s3cmd:
-```shell
-s3cmd ls s3://NDAR_Central/
-```
 
-
+1. On Linx/Unix
+    ```shell
+    s3cmd --config ~/ndar_toolkit/.s3cfg ls s3://NDAR_Central/
+    ```
+2. On Windows
+   ```shell
+    python %HOME%/ndar_toolkit/s3cmd --config %HOME%/ndar_toolkit/.s3cfg ls s3://NDAR_Central/
+    ```
--- ndar_update_keys.py
+++ ndar_update_keys.py
@@ -25,54 +25,65 @@ NOTES:
 - Downloading and persisting data, as well as any kind of redistribution is strictly prohibited.
 - Please read the NDAR Omics policy as it applies to Data Download https://ndar.nih.gov/faq.html#faq64
 """  
-
+from os.path import expanduser 
 import sys
 import stat
 import fileinput
 import getpass
 from subprocess import call
-import ConfigParser, os
-import urllib2
+try:
+    import ConfigParser
+except ImportError:
+    import configparser as ConfigParser
+import os
+try:
+    import urllib2
+except ImportError:
+    from urllib import request
 import zipfile
 import tarfile
 
 # Default home directory, set this to the home folder for the user you plan to use, (i.e., ec2-uesr, ubuntu, etc.)
-home = '/home/ec2-user'
+#home = '/home/ec2-user'
 #home = '/home/ubuntu'
 #home = '/home/mobaxterm'
 
+
+home = expanduser("~")
+print ('Home is %s' %home) 
 def download_file( url, dest ):
-    f = urllib2.urlopen( url )
+    if sys.version_info[0] < 3:    
+        f = urllib2.urlopen( url )
+    else:
+        f = request.urlopen( url )
     with open( dest, "wb") as local_file:
         local_file.write(f.read())
 
 def unzip_file( filename ):
-    fh = open( filename )
-    z = zipfile.ZipFile(fh)
+    z = zipfile.ZipFile(filename)
     for name in z.namelist():
-        outpath = home + "/"
+        outpath = os.path.normpath(home + "/ndar_toolkit")
         z.extract( name, outpath )
-    fh.close()
+    z.close()
     os.remove( filename )
 
 def shell_source(script):
-    """Sometime you want to emulate the action of "source" in bash,
-    settings some environment variables. Here is a way to do it."""
-    import subprocess, os
-    pipe = subprocess.Popen(". %s; env" % script, stdout=subprocess.PIPE, shell=True)
-    output = pipe.communicate()[0]
-    env = dict((line.split("=", 1) for line in output.splitlines()))
-    os.environ.update(env)
+   f = open (os.path.normpath( home + '/ndar_toolkit/ndar_update_keys.sh'), 'r')
+   line = f.readline()
+   ndar_username = line.split("=",1)[1]
+   line = f.readline()
+   ndar_password = line.split("=",1)[1]
+   return ndar_username.strip(), ndar_password.strip()  
 
 def create_default_config(home, config_file):
-    f = open (home + config_file, 'w')
+    f = open (os.path.normpath(home + config_file), 'wt')
     f.write('[default]\n')
     f.close()
 
 def write_aws_config(home, config_file, profile):
     config_aws_cli = ConfigParser.ConfigParser()
-    if (os.path.isfile(home + config_file)):
-        config_aws_cli.read(home + config_file)
+    if (os.path.isfile(os.path.normpath(home + config_file))):
+        config_aws_cli.read(os.path.normpath(home + config_file))
         if not config_aws_cli.has_section(profile):
             config_aws_cli.add_section(profile)
         config_aws_cli.set(profile, 'aws_access_key_id', myvars["accessKey"])
@@ -81,7 +92,7 @@ def write_aws_config(home, config_file, profile):
         config_aws_cli.set(profile, 'region', 'us-east-1')
     else:
         create_default_config(home,config_file)
-        config_aws_cli.read( home + config_file)
+        config_aws_cli.read( os.path.normpath(home + config_file))
         aws_cli_info = {
             'aws_access_key_id': ' #Add your AWS access key ',
             'aws_secret_access_key': ' #Add your AWS secret key ',
@@ -96,20 +107,20 @@ def write_aws_config(home, config_file, profile):
         config_aws_cli.set(profile, 'aws_session_token', myvars["sessionToken"])
         config_aws_cli.set(profile, 'region', 'us-east-1')
 
-    with open(home + config_file, 'wb') as configfile:
+    with open(os.path.normpath(home + config_file), 'wt') as configfile:
         config_aws_cli.write(configfile)
 
 def write_s3cmd_config(home, config_file):
     config_s3cmd = ConfigParser.ConfigParser()
 
-    if (os.path.isfile( home + config_file )):
-        config_s3cmd.read(home + config_file)
+    if (os.path.isfile( os.path.normpath(home + config_file ))):
+        config_s3cmd.read(os.path.normpath(home + config_file))
         config_s3cmd.set('default', 'access_key', myvars["accessKey"])
         config_s3cmd.set('default', 'secret_key', myvars["secretKey"])
         config_s3cmd.set('default', 'access_token', myvars["sessionToken"])
     else:
         create_default_config(home, config_file)
-        config_s3cmd.read( home + config_file)
+        config_s3cmd.read( os.path.normpath(home + config_file))
         s3cmd_info = {
             'access_key': myvars["accessKey"],
             'secret_key': myvars["secretKey"],
@@ -160,89 +171,91 @@ def write_s3cmd_config(home, config_file):
         #config_s3cmd.add_section('default')
         for key in s3cmd_info.keys():
             config_s3cmd.set('default', key, s3cmd_info[key])
-    with open( home + '/.s3cfg', 'wb') as configfile:
+    with open( os.path.normpath(home + '/ndar_toolkit/.s3cfg'), 'wt') as configfile:
         config_s3cmd.write(configfile)
 
 if __name__ == '__main__':
 
 
-    if (os.path.isfile( home + '/ndar_update_keys.sh' )):
-        shell_source(home + '/ndar_update_keys.sh')
+    if not os.path.exists (os.path.normpath(home + "/ndar_toolkit")):
+        os.makedirs (os.path.normpath(home + "/ndar_toolkit"))
+
+    if (os.path.isfile( os.path.normpath(home + '/ndar_toolkit/ndar_update_keys.sh' ))):
+        ndar_username, ndar_password = shell_source(os.path.normpath(home + '/ndar_toolkit/ndar_update_keys.sh'))
     
     if (os.environ.get('ndar_username')):
         ndar_username = os.environ.get('ndar_username')
-    else:
-        ndar_username = raw_input("ndar_username not set, please enter it now: ")
-        f = open( home + '/ndar_update_keys.sh', 'wb')
-        f.write('export ndar_username=' + ndar_username + '\n')
+    try: 
+        ndar_username
+    except NameError:
+        if sys.version_info[0] < 3:
+            ndar_username = raw_input("ndar_username not set, please enter it now: ")
+        else:
+            ndar_username = input("ndar_username not set, please enter it now: ")
+        f = open( os.path.normpath(home + '/ndar_toolkit/ndar_update_keys.sh'), 'wt')
+        f.write('export ndar_username=' + str(ndar_username) + '\n' )
         #ndar_username = 'USERNAME' # Replace USERNAME with your NDAR username
     
     if (os.environ.get('ndar_password')):
         ndar_password = os.environ.get('ndar_password')
-    else:
+    try:
+        ndar_password
+    except NameError:
         ndar_password = getpass.getpass("ndar_password is not set, please enter it now: ")
-        f.write('export ndar_password=' + ndar_password + '\n')
+        f.write('export ndar_password=' + str(ndar_password) + '\n')
         f.close()
         #ndar_password = 'PASSWORD' # Replace password with your NDAR password
     
     
     # Creates location for aws credentials files
-    if not os.path.exists ( home + '/.aws/'):
-        os.makedirs ( home + '/.aws/')
+    if not os.path.exists ( os.path.normpath(home + '/ndar_toolkit/.aws/')):
+        os.makedirs ( os.path.normpath(home + '/ndar_toolkit/.aws/'))
         #os.makedirs ( '/root/.aws/' )
     
-    if not (os.path.isfile( home + '/downloadmanager.jar' )):
-        download_file("https://ndar.nih.gov/jnlps/download_manager_client/downloadmanager.zip", home + '/downloadmanager.zip')
-        unzip_file( home + "/downloadmanager.zip" )
+    if not (os.path.isfile( os.path.normpath(home + '/ndar_toolkit/downloadmanager.jar' ))):
+        download_file("https://ndar.nih.gov/jnlps/download_manager_client/downloadmanager.zip", os.path.normpath(home + '/ndar_toolkit/downloadmanager.zip'))
+        unzip_file( os.path.normpath(home + "/ndar_toolkit/downloadmanager.zip" ))
     
-    if not (os.path.isfile( home + '/s3cmd-master/s3cmd' )):
+    if not (os.path.isfile( os.path.normpath(home + '/s3cmd-master/s3cmd' ))):
         download_file("https://github.com/s3tools/s3cmd/archive/master.zip", home + '/s3cmd-master.zip')
-        unzip_file( home + '/s3cmd-master.zip')
-        for i, line in enumerate(fileinput.input( home + '/s3cmd-master/S3/S3.py', inplace=1)):
+        unzip_file( os.path.normpath(home + '/s3cmd-master.zip'))
+        for i, line in enumerate(fileinput.input( os.path.normpath(home + '/ndar_toolkit/s3cmd-master/S3/S3.py'), inplace=1)):
             sys.stdout.write(line.replace('self.s3.config.role_refresh()', 
                                           '# Role refresh is with NDAR Download Manager\n# self.s3.config.role_refresh()')) 
-        st = os.stat(home + '/s3cmd-master/s3cmd')
-        os.chmod(home + '/s3cmd-master/s3cmd', st.st_mode | stat.S_IEXEC | stat.S_IXGRP | stat.S_IXOTH)
+        st = os.stat(os.path.normpath(home + '/ndar_toolkit/s3cmd-master/s3cmd'))
+        os.chmod(os.path.normpath(home + '/ndar_toolkit/s3cmd-master/s3cmd'), st.st_mode | stat.S_IEXEC | stat.S_IXGRP | stat.S_IXOTH)
+        
+        if not os.path.exists ( os.path.normpath(home + '/bin/' )):
+            os.makedirs ( os.path.normpath(home + '/bin/' ))
     
-        if os.path.lexists('/bin/s3cmd'):
-            os.remove('/bin/s3cmd')
-        os.symlink(home + '/s3cmd-master/s3cmd', '/bin/s3cmd')
-    
-    devnull = open('/dev/null', 'w')
-    print "Generating new keys and updating config files"
-    
-    status=call("java -jar " + home + "/downloadmanager.jar" + " --generate awskeys.txt "
-                + "--username '" + ndar_username + "' "
-                + "--password '" + ndar_password +"' 2>&1 /dev/null",
+        if os.path.lexists( os.path.normpath(home + '/bin/s3cmd')):
+            os.remove( os.path.normpath(home + '/bin/s3cmd'))
+        try:
+            os.symlink(os.path.normpath(home + '/ndar_toolkit/s3cmd-master/s3cmd'), home + '/bin/s3cmd')
+        except:
+            pass
+    devnull = open(os.devnull, 'w')
+    print ("Generating new keys and updating config files")
+    os.chdir( os.path.normpath(home + "/ndar_toolkit"))
+    status=call('java -jar ' + os.path.normpath(home + '/ndar_toolkit/downloadmanager.jar') + ' --generate awskeys.txt '
+                + '--username "' + ndar_username + '" '
+                + '--password "' + ndar_password +'" 2>&1 /dev/null',
+                #shell=True)
                 shell=True, stdout=devnull, stderr=devnull)
 
-    #if using Windows, it may be necessar to explicitly specify the Windows path to the jar file
-    #status=call("java -jar " + "C:/Users/obenshaindw/MobaXterm" +  "/downloadmanager.jar" + " --generate awskeys.txt "
-    #            + "--username '" + ndar_username + "' "
-    #            + "--password '" + ndar_password +"' 2>&1 /dev/null",
-	#	shell=True, stdout=devnull, stderr=devnull)
-
-
     myvars={}
-    with open (home + "/awskeys.txt") as keysfile:
+    with open (os.path.normpath(home + "/ndar_toolkit/awskeys.txt")) as keysfile:
     	for line in keysfile:
-		#line = line.replace(r'\r', '')
-    		name, var = line.partition("=")[::2]
-		var = var.replace('\r', '')
-    		myvars[name.strip()] = str(var)
+            #line = line.replace(r'\r', '')
+            name, var = line.partition("=")[::2]
+            var = var.replace('\r', '')
+            myvars[name.strip()] = str(var)
     
     os.environ["AWS_ACCESS_KEY_ID"] = myvars["accessKey"]
     os.environ["AWS_SECRET_ACCESS_KEY"] = myvars["secretKey"]
     os.environ["AWS_SESSION_TOKEN"] = myvars["sessionToken"]
 
     #Additional entries may be necessary depending on environment, to write configuration file to home directory
-
-    write_aws_config('/home/ec2-user', '/.aws/credentials', 'NDAR')
-    write_aws_config('/root', '/.aws/credentials', 'NDAR')
-    #write_aws_config('/home/mobaxterm', '/.aws/credentials', 'NDAR')
-    write_aws_config('/home/ec2-user', '/.aws/config', 'profile NDAR')
-    write_aws_config('/root', '/.aws/config', 'profile NDAR')
-    #write_aws_config('/home/mobaxterm', '/.aws/config', 'profile NDAR')
-    write_s3cmd_config('/home/ec2-user', '/.s3cfg')
-    write_s3cmd_config('/root', '/.s3cfg')
-    #write_s3cmd_config('/home/mobaxterm', '/.s3cfg')
+    write_aws_config( home, '/ndar_toolkit/.aws/credentials', 'NDAR')
+    write_aws_config( home, '/ndar_toolkit/.aws/config', 'profile NDAR')
+    write_s3cmd_config( home, '/ndar_toolkit/.s3cfg')
